39ead9c756fa85047cdaf60c076001960f076452
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2025-01-29 09:14:31 +0800

    Modified: samesite to lax

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -50,7 +50,7 @@ export const authenticate = (req, res) => {
   res.cookie("access_token", access_token, {
     httpOnly: true, // Makes the cookie inaccessible to JavaScript
     secure: process.env.ENVIRONMENT === "production", // Use HTTPS in production
-    sameSite: "strict", // Prevents CSRF attacks
+    sameSite: "lax", // Prevents CSRF attacks
     // maxAge: 30 * 24 * 60 * 60 * 1000, // Expires in 30 days
     // maxAge: 30 * 1000, // Expires in 30 seconds
     maxAge: 60 * 60 * 1000, // Expires in 1 hour
