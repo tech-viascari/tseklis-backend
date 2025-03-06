869d6cb573fe6a891f2e33d6d64fe09d358a3420
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2025-03-06 13:25:42 +0800

    Fixed: Legal Entity

--- src/controllers/legal_entities_controller.js
+++ src/controllers/legal_entities_controller.js
@@ -2,7 +2,7 @@ import LegalEntities from "../models/LegalEntities.js";
 
 export const getAllLegalEntities = async (req, res) => {
   try {
-    let legalEntities = await new LegalEntities().findAll();
+    let legalEntities = await new LegalEntities().fetchAll();
     res.status(200).json(legalEntities);
   } catch (error) {
     res.status(500).json({
@@ -13,59 +13,72 @@ export const getAllLegalEntities = async (req, res) => {
   }
 };
 
+export const addLegalEntity = async (req, res) => {
+  const { entity_details } = req.body;
+
+  try {
+    let legalEntity = new LegalEntities();
+    legalEntity.entity_details = entity_details;
+    legalEntity.entity_logo = req.entity_logo;
+
+    const newLegalEntity = await legalEntity.add();
+
+    if (newLegalEntity) {
+      return res.status(200).json({ newLegalEntity });
+    } else {
+      throw Error("Failed to insert the record.");
+    }
+  } catch (error) {
+    return res.status(500).json({ status: "failed", error: error.message });
+  }
+};
+
 export const getLegalEntity = async (req, res) => {
   const { entity_id } = req.params;
   try {
-    let legalEntity = await LegalEntities.find(entity_id);
-    res.status(200).json(legalEntity);
+    const entity = await new LegalEntities().fetch({ entity_id });
+    if (entity) {
+      return res.status(200).json({ entity });
+    } else {
+      throw Error("Record not found.");
+    }
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: "Internal Server Error",
-      err: error.message,
-    });
+    return res.status(500).json({ status: "failed", error: error.message });
   }
 };
 
-export const createLegalEntity = async (req, res) => {
-  const legalEntity = req.body;
+export const updatePermission = async (req, res) => {
+  // const { entity_id } = req.params;
+
+  // console.log(entity_id);
   try {
-    let entity_id = await new LegalEntities().create(legalEntity);
-    res.status(200).json({ entity_id });
+    // const entity = await new LegalEntities().fetch({ entity_id });
+    // if (entity) {
+    //   const newPermission = await new LegalEntities({ ...req.body }).update();
+    //   if (newPermission) {
+    //     return res.status(200).json({ status: "success", newPermission });
+    //   } else {
+    //     throw Error("Failed to update the record.");
+    //   }
+    // } else {
+    //   throw Error("LegalEntities ID is not found.");
+    // }
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: "Internal Server Error",
-      err: error.message,
-    });
+    return res.status(500).json({ status: "failed", error: error.message });
   }
 };
 
-export const updateLegalEntity = async (req, res) => {
+export const deletePermission = async (req, res) => {
   const { entity_id } = req.params;
-  const legalEntity = req.body;
-  try {
-    let updated = await LegalEntities.update(entity_id, legalEntity);
-    res.status(200).json({ updated });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: "Internal Server Error",
-      err: error.message,
-    });
-  }
-};
 
-export const deleteLegalEntity = async (req, res) => {
-  const { entity_id } = req.params;
   try {
-    let deleted = await LegalEntities.delete(entity_id);
-    res.status(200).json({ deleted });
+    const entity = await new LegalEntities().delete({ entity_id });
+    if (entity) {
+      return res.status(200).json({ entity });
+    } else {
+      throw Error("Failed to delete the record.");
+    }
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: "Internal Server Error",
-      err: error.message,
-    });
+    return res.status(500).json({ status: "failed", error: error.message });
   }
 };
--- src/models/LegalEntities.js
+++ src/models/LegalEntities.js
@@ -17,6 +17,7 @@ const OfficerInformationState = {
 };
 // business types are composed of Sole, Partnership, and Corporation
 const BusinessTypes = ["Corporation"];
+
 const ClientTypes = [
   "Viascari Group of Companies",
   "Computershare Clients",
@@ -30,61 +31,110 @@ const CompanyTypes = [
   "Stock Foreign Representative Office",
 ];
 
+const LegalEntityDetailsState = {
+  //corporation muna ito
+  business_type: "Corporation",
+  client_type: "Viascari Group of Companies",
+  company_name: "",
+  company_address: "",
+  type_of_company: "Non Stock",
+  corporate_tin: "",
+  sec_registration_number: "",
+  official_email: "",
+  alternative_email: "",
+  official_contact_number: "",
+  alternative_contact_number: "",
+  officer_information: [],
+};
+
+const GdriveFolderState = {
+  root_folder_id: "",
+  final_docs_id: "",
+  sec_cert: "",
+  articles_of_incorporation: "",
+  by_laws: "",
+  bir_cor: "",
+  lgu_business_permit: "",
+};
+
+const LegalEntityState = {
+  entity_id: "",
+  entity_details: LegalEntityDetailsState,
+  entity_logo: "",
+  status: "Active",
+  gdrive_folder: GdriveFolderState,
+  created_at: new Date(),
+  updated_at: new Date(),
+};
+
 class LegalEntities {
   constructor({
     entity_id = "",
-    business_type = BusinessTypes,
-    client_type = ClientTypes,
-    company_name = "",
-    company_address = "",
-    type_of_company = CompanyTypes,
-    corporate_tin = "",
-    sec_registration_number = "",
-    official_email = "",
-    alternative_email = "",
-    official_contact_number = "",
-    alternative_contact_number = "",
-    company_logo = "",
-    officer_information = OfficerInformationState,
+    entity_details = LegalEntityDetailsState,
+    entity_logo = "",
+    status = "Active",
+    gdrive_folder = GdriveFolderState,
     created_at = new Date(),
     updated_at = new Date(),
   } = {}) {
     this.entity_id = entity_id;
-    this.business_type = business_type;
-    this.client_type = client_type;
-    this.company_name = company_name;
-    this.company_address = company_address;
-    this.type_of_company = type_of_company;
-    this.corporate_tin = corporate_tin;
-    this.sec_registration_number = sec_registration_number;
-    this.official_email = official_email;
-    this.alternative_email = alternative_email;
-    this.official_contact_number = official_contact_number;
-    this.alternative_contact_number = alternative_contact_number;
-    this.company_logo = company_logo;
-    this.officer_information = officer_information;
+    this.entity_details = entity_details;
+    this.entity_logo = entity_logo;
+    this.status = status;
+    this.gdrive_folder = gdrive_folder;
     this.created_at = created_at;
     this.updated_at = updated_at;
   }
 
-  async create(legalEntity) {
-    return db(DB_NAME).insert(legalEntity).returning(DB_PRIMARY_KEY);
+  // Fetch all records
+  async fetchAll() {
+    return await db(DB_NAME).select("*");
   }
 
-  async update(entity_id, legalEntity) {
-    return db(DB_NAME).where(DB_PRIMARY_KEY, entity_id).update(legalEntity);
+  // Fetch a record by ID
+  async fetch(columnNames) {
+    return await db(DB_NAME).where(columnNames).first();
   }
 
-  async delete(entity_id) {
-    return db(DB_NAME).where(DB_PRIMARY_KEY, entity_id).del();
+  // Add a new record
+  async add() {
+    // Exclude the `entity_id`, `created_at`,`updated_at` from the insert data
+    const { entity_id, created_at, updated_at, ...dataToInsert } = this;
+    return await db(DB_NAME).insert(dataToInsert).returning(Object.keys(this));
   }
 
-  async find(entity_id) {
-    return db(DB_NAME).where(DB_PRIMARY_KEY, entity_id).first();
+  // Update a record
+  async update() {
+    // Exclude the `created_at`,`updated_at` from the update data
+    const { created_at, updated_at, ...dataToUpdate } = this;
+    const fieldsToUpdate = LegalEntities.getUpdateFields(dataToUpdate);
+    if (Object.keys(fieldsToUpdate).length > 0) {
+      return await db(DB_NAME)
+        .where({ entity_id: this.entity_id })
+        .update(fieldsToUpdate)
+        .returning(Object.keys(this));
+    }
+    return [this];
   }
 
-  async findAll() {
-    return db(DB_NAME);
+  // Delete a record by ID
+  async delete(id) {
+    return await db(DB_NAME).where(id).del();
+  }
+
+  // Static method to prepare fields for updates
+  static getUpdateFields(instance) {
+    const updates = {};
+    for (const key in instance) {
+      if (
+        instance[key] !== undefined &&
+        key !== DB_PRIMARY_KEY &&
+        instance[key] !== null
+      ) {
+        updates[key] = instance[key] === "" ? "" : instance[key];
+      }
+    }
+    return updates;
   }
 }
 
